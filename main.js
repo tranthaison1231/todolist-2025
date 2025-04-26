b4ef82b3541af65581683112351de5784bd16de5
Son Tran <son.tran@enouvo.com>
2025-04-26 21:24:19 +0700

    feat(master): update todo

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+let TODOS = [
+  {
+    id: 1,
+    content: "Buy groceries",
+    completed: false,
+  },
+  {
+    id: 2,
+    content: "Buy car",
+    completed: true,
+  },
+  {
+    id: 3,
+    content: "Buy house",
+    completed: false,
+  },
+  {
+    id: 4,
+    content: "Buy clock",
+    completed: false,
+  },
+];
+
 const todoInput = document.getElementById("todo-input");
 const todoButton = document.getElementById("todo-button");
 const todoList = document.getElementById("todo-list");
 const todoCount = document.getElementById("todo-count");
 
-function addTodo() {
+function renderTodo(todo) {
   const todoItem = document.createElement("li");
+
+  todoItem.setAttribute("data-id", todo.id);
 
   todoItem.classList.add(
     "flex",
@@ -18,20 +43,50 @@ function addTodo() {
   );
 
   todoItem.innerHTML = `
-      <input type="checkbox" class="h-5 w-5 rounded border-gray-300 text-purple-600 focus:ring-purple-500">
-      <span class="ml-3 flex-1 text-gray-800">${todoInput.value}</span>
+      <input type="checkbox" ${
+        todo.completed ? "checked" : ""
+      } class="h-5 w-5 rounded border-gray-300 text-purple-600 focus:ring-purple-500">
+      <span class="ml-3 flex-1 text-gray-800">${todo.content}</span>
       <button class="text-gray-400 hover:text-red-500 transition-colors duration-200">
         <i class="fas fa-trash"></i>
       </button>
     `;
 
   todoList.appendChild(todoItem);
+}
 
-  const todoLength = todoList.children.length;
-  todoCount.innerHTML = `${todoLength} items left`;
+function renderTodoList(todos) {
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    renderTodo(todo);
+  });
 
+  const activeTodos = todos.filter((todo) => !todo.completed);
+
+  todoCount.innerHTML = `${activeTodos.length} items left`;
+}
+
+function addTodo() {
+  if (todoInput.value === "") return;
+
+  const todo = {
+    id: TODOS.length + 1,
+    content: todoInput.value,
+    completed: false,
+  };
+
+  TODOS.push(todo);
+  renderTodoList(TODOS);
   todoInput.value = "";
 }
+
+todoInput.addEventListener("keyup", (e) => {
+  if (e.target.value === "") {
+    todoButton.setAttribute("disabled", "true");
+  } else {
+    todoButton.removeAttribute("disabled");
+  }
+});
 
 todoInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
@@ -45,20 +100,27 @@ todoButton.addEventListener("click", () => {
 
 todoList.addEventListener("click", (e) => {
   if (e.target.tagName === "I") {
-    e.target.parentElement.parentElement.remove();
+    const todoId = e.target.parentElement.parentElement.getAttribute("data-id");
+
+    TODOS = TODOS.filter((todo) => todo.id !== Number(todoId));
+
+    renderTodoList(TODOS);
   }
 });
 
 todoList.addEventListener("change", (e) => {
   if (e.target.tagName === "INPUT") {
-    let count = 0;
+    const todoId = e.target.parentElement.getAttribute("data-id");
 
-    for (let i = 0; i < todoList.children.length; i++) {
-      if (!todoList.children[i].firstElementChild.checked) {
-        count++;
+    TODOS = TODOS.map((todo) => {
+      if (todo.id === Number(todoId)) {
+        todo.completed = e.target.checked;
       }
-    }
+      return todo;
+    });
 
-    todoCount.innerHTML = `${count} items left`;
+    renderTodoList(TODOS);
   }
 });
+
+renderTodoList(TODOS);
